8f8abd45a75ea1564057d18bcc2751692dbba14d
David Crenin <david.crenin@gmail.com>
2019-08-31 14:42:22 +0200

    test

--- rossisite/settings.py
+++ rossisite/settings.py
@@ -13,11 +13,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 
 # settings.py
-from dotenv import load_dotenv
-load_dotenv()
-
-# OR, the same with increased verbosity
-load_dotenv(verbose=True)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -27,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY_DJANGO')
+SECRET_KEY = os.environ('SECRET_KEY_DJANGO')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -87,8 +82,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'derossidb',
-        'USER': os.getenv('USER_DB'),
-        'PASSWORD': os.getenv('PASSWORD_DB'),
+        'USER': os.environ('USER_DB'),
+        'PASSWORD': os.environ('PASSWORD_DB'),
         'HOST': 'localhost',
         'PORT': '5433',
     }
@@ -150,6 +145,7 @@ STATICFILES_FINDERS = [
 ]
 SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR,'assets')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
