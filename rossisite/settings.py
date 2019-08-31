6a7f0992b0a19aec6f8bbff2e2c05c8bb66ac50b
David Crenin <david.crenin@gmail.com>
2019-08-31 14:57:39 +0200

    test

--- rossisite/settings.py
+++ rossisite/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 
 # settings.py
+from dotenv import load_dotenv
+load_dotenv()
+
+# OR, the same with increased verbosity
+load_dotenv(verbose=True)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -22,10 +27,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ('SECRET_KEY_DJANGO')
+SECRET_KEY = os.getenv('SECRET_KEY_DJANGO')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -82,8 +87,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'derossidb',
-        'USER': os.environ('USER_DB'),
-        'PASSWORD': os.environ('PASSWORD_DB'),
+        'USER': os.getenv('USER_DB'),
+        'PASSWORD': os.getenv('PASSWORD_DB'),
         'HOST': 'localhost',
         'PORT': '5433',
     }
@@ -122,29 +127,31 @@ USE_L10N = True
 
 USE_TZ = True
 
+PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 
 STATICFILES_DIRS = [
-  os.path.join(BASE_DIR, 'assets/')
+  os.path.join(PROJECT_ROOT, 'assets/')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media_cdn')
 MEDIA_URL = '/media_cdn/'
 
 
-#  Add configuration for static files storage using whitenoise
-# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     'compressor.finders.CompressorFinder',
     'sass_processor.finders.CssFinder',
 ]
-SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR,'assets')
+SASS_PROCESSOR_ROOT = os.path.join(PROJECT_ROOT,'assets')
+
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
