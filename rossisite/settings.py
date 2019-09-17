876c4747b1696f7a9ce92aef21d1d53bf375d02c
David Crenin <david.crenin@gmail.com>
2019-09-17 10:24:18 +0200

    test

--- rossisite/settings.py
+++ rossisite/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sass_processor',
-    'cloudinary',
 ]
 
 MIDDLEWARE = [
---
+++ static/pages/js/flexslider.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $('#carousel').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: false,
+    slideshow: false,
+    itemWidth: 100,
+    itemMargin: 5,
+    asNavFor: '#slider',
+    directionNav:false,
+  });
+
+  $('#slider').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: false,
+    slideshow: false,
+    sync: "#carousel",
+    directionNav:false,
+  });
+  });
