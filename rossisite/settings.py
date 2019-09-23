1a91d8e3814bcfa61e7997b192524136b7a02228
David Crenin <david.crenin@gmail.com>
2019-09-23 11:54:49 +0200

    test

--- rossisite/settings.py
+++ rossisite/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sass_processor',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -134,12 +135,24 @@ STATICFILES_DIRS = [
   os.path.join(BASE_DIR, 'pages/static'),
 ]
 SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR,'static')
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
-MEDIA_URL = '/media_cdn/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
+MEDIA_URL = '/media/'
+
+
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_REGION_NAME = 'eu-west-3'
+AWS_DEFAULT_ACL = None
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 
 
 
